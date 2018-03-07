a6e88dc4718301c88c008fd896c51f6470ad0094
junaid <junaid1460@gmail.com>
2018-03-07 09:56:37 +0530

    create global var storage file

---
+++ lib/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final  firebaseAuth = FirebaseAuth.instance;
+
+
+class AppService {
+  static BuildContext context;
+}
+
